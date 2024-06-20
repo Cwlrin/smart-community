e7b76c1bac941104bec664f9b68302e8026952a5
桜小路七葉 <Nana7ha@outlook.com>
2024-06-20 21:20:25 +0800

    feat(card)：实现初始月卡列表 API 和 UI
    添加新的 card.js API 端点用于获取月卡列表，并在 CarCard index.vue 中为管理服务器实现相应的 UI。包括搜索功能、表格显示和基本样式。

---
+++ server-for-admin/src/api/card.js
@@ -0,0 +1,14 @@
+import request from '@/utils/request'
+
+/**
+ * 获取月卡列表
+ * @param { page, pageSize } params
+ * @returns
+ */
+export function getCardListAPI(params) {
+  return request({
+    url: '/parking/card/list',
+    method: 'get',
+    params
+  })
+}
