da3c61ea4b343fdd7f167bdc3dd00085d13dfc16
桜小路七葉 <Nana7ha@outlook.com>
2024-06-23 20:37:19 +0800

    feat(enterprise)：添加企业列表 API 和相关 UI 组件
    在 enterprise.js 模块中实现新的 getEnterpriseListAPI 以获取企业列表。在 index.vue 中，添加相应的 UI 组件，用于搜索、表格显示和企业数据分页。设计
    包括搜索容器、包含企业详细信息的表格和
    用于导航的分页栏。

--- server-for-admin/src/views/Park/Enterprise/index.vue
+++ server-for-admin/src/views/Park/Enterprise/index.vue
@@ -1,14 +1,109 @@
 <template>
-  <div class="enterprise-container">
-    企业管理
+  <div class="building-container">
+    <!-- 搜索区域 -->
+    <div class="search-container">
+      <div class="search-label">企业名称：</div>
+      <el-input class="search-main" clearable placeholder="请输入内容" />
+      <el-button type="primary">查询</el-button>
+    </div>
+    <div class="create-container">
+      <el-button type="primary">添加企业</el-button>
+    </div>
+    <!-- 表格区域 -->
+    <div class="table">
+      <el-table :data="enterpriseList" style="width: 100%">
+        <el-table-column label="序号" type="index" />
+        <el-table-column label="企业名称" prop="name" width="320" />
+        <el-table-column label="联系人" prop="contact" />
+        <el-table-column
+          label="联系电话"
+          prop="contactNumber"
+        />
+        <el-table-column label="操作">
+          <template #default="scope">
+            <el-button size="mini" type="text">添加合同</el-button>
+            <el-button size="mini" type="text">查看</el-button>
+            <el-button size="mini" type="text">编辑</el-button>
+            <el-button size="mini" type="text">删除</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+    </div>
+    <div class="page-container">
+      <el-pagination
+        layout="total, prev, pager, next"
+        :total="total"
+        :page-size="params.pageSize"
+        @current-change="handleChange"
+      />
+    </div>
   </div>
 </template>
 
 <script>
+import { getEnterpriseListAPI } from '@/api/enterprise'
+
 export default {
-  name: 'Building'
+  name: 'EnterPrise',
+  data() {
+    return {
+      params: {
+        name: '',
+        page: 1,
+        pageSize: 10
+      },
+      enterpriseList: [],
+      total: 0
+    }
+  },
+  mounted() {
+    this.getEnterpriseList()
+  },
+  methods: {
+    handleChange(val) {
+      this.params.page = val
+      this.getEnterpriseList()
+    },
+    async getEnterpriseList() {
+      const res = await getEnterpriseListAPI(this.params)
+      this.enterpriseList = res.data.rows
+      this.total = res.data.total
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
+.department-container {
+  padding: 10px;
+}
+
+.search-container {
+  display: flex;
+  align-items: center;
+  border-bottom: 1px solid rgb(237, 237, 237, .9);;
+  padding-bottom: 20px;
+
+  .search-label {
+    width: 100px;
+  }
+
+  .search-main {
+    width: 220px;
+    margin-right: 10px;
+  }
+}
+
+.create-container {
+  margin: 10px 0;
+}
+
+.page-container {
+  padding: 4px 0;
+  text-align: right;
+}
+
+.form-container {
+  padding: 0 80px;
+}
 </style>
